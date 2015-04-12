f3bd236ff05a0b2ee09114c5a54fbc946ac09d3a
Joon Hyung Ahn <jahn45@siebl-0403-40.ews.illinois.edu>
2015-04-12 18:54:59 -0500

    yay

--- node.py
+++ node.py
@@ -12,10 +12,11 @@ import globals
 
 class Node:
 	keys = [] * 256
-	ft = [] * 7
+	ft = [] * 8
 	num_ft = 0
-	sock = [] * 7
+	sock = [None] * 8
 	predecessor = None
+	coord = None
 
 	# join()
 	def __init__(self, node_id):
@@ -23,55 +24,64 @@ class Node:
 
 		#node 0 will be initialized with all keys
 		if node_id is 0:
-			for x in range(0, 256):
-				self.keys[x] = 1
-			for x in range(0, 8):
-				self.ft[x] = node_id
+			self.keys = [1] * 256
+			self.ft = [node_id] * 8
 			self.predecessor = node_id
 
 		else:
-			# setup connection
-			self.initConn()
+			# setup connection to 0th node
+			self.init_base()
 
 			#send message to node 0 to help find myNode find its predecessor
-			print "----------------------------------"
-			print self.sock
-			print "----------------------------------"
 			self.sock[0].sendall("find_successor " + str(self.node_id))
 			#1. initialize the finger table
 			#2. initialize predecessor
 			#3. Update the predecessor of existing nodes
 			#4. Update the finger table of existing nodes
 			#5. Transfer keys to node_id
-
-		#after setting up is complete, setup connection to appropriate nodes + coordinator
-
-		#client thread - sets up connection to other nodes
-		client_t=threading.Thread(target = self.clientThread, args = (node_id,))
-		client_t.start()
 		
+		# initialize finger table
+		self.init_finger_table()
+
 		#server thread - receives connection from other nodes
-		server_t=threading.Thread(target = self.serverThread, args = (node_id,))
+		server_t=threading.Thread(target = self.serverThread, args = ())
 		server_t.start()
 
 
+	## initialize conneciton to 0th node
+	def init_base(self):
+		try:
+			self.sock[0] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		except socket.error, msg:
+			print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+			sys.exit();
+		
+		try:
+			self.sock[0].connect((globals.coord_ip , globals.coord_port + 1)) 
+		except socket.error, msg:
+			print 'Failed to connect socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+			sys.exit();
 
-	#setup a connection to all nodes in finger table + coordinator
-	def initConn(self):
-		print 'Setting up node'
-		global c_client
-
+	#setup a connection to coordinator
+	def init_coord(self):
 		#setup connection to coordinator
 		try:
 			#create an AF_INET, STREAM socket (TCP)
-			c_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			self.coord = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		except socket.error, msg:
 			print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
 			sys.exit();
 
-		c_client.connect((globals.coord_ip , globals.coord_port))
+		self.coord.connect((globals.coord_ip , globals.coord_port))
 		print 'Socket Connected to ' + globals.coord_ip
 
+		#register client to the server
+		if(self.coord.sendall("registration " + str(self.node_id))==None):
+			print '%s connected to server' % self.node_id
+		else:
+			print 'client registration incomplete'
+
+	def init_finger_table(self):
 		#setup connection to nodes in fingertable
 		for x in range(0, self.num_ft):
 			try:
@@ -82,16 +92,10 @@ class Node:
 				sys.exit();
 			#node port num = 8000 + node_id
 			self.sock[x].connect(globals.coord_ip, globals.coord_port + self.ft[x])
-			print 'Socket Connected to node' + self.ft[x]
-
-		#register client to the server
-		if(c_client.sendall("registration " + str(self.node_id))==None):
-			print '%s connected to server' % self.node_id
-		else:
-			print 'client registration incomplete'
+			print 'Socket Connected to node' + self.ft[x]		
 
 	#receives messages from other nodes
-	def recvThread(node_id):
+	def recvThread(self, conn):
 		#continuously receive data from the nodes
 		while(globals.keep_alive):
 			data = conn.recv(1024)
@@ -99,12 +103,12 @@ class Node:
 			buf = data.split(' ')
 			#if node 0 asked to find a node's successor
 			if(buf[0] == "find_successor"):
-				find_successor(buf[1])
+				self.find_successor(buf[1])
 
 	#receives connection from other nodes
-	def serverThread(node_id):
+	def serverThread(self):
 		s_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		node_port = globals.coordinator_port + node_id
+		node_port = globals.coord_port + self.node_id + 1
 		try: # setup server socket
 			s_server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 			s_server.bind((globals.coord_ip, node_port))
@@ -121,28 +125,24 @@ class Node:
 		while(globals.keep_alive):
 			conn, addr = s_server.accept()
 			print 'Connected With '  + addr[0] + ':' + str(addr[1])
-			thread.start_new_thread(recvThread, (conn, str(addr[1])))
+			thread.start_new_thread(self.recvThread, (conn,))
 
 		conn.close()
 		s_server.close()
 
-	#thread representing a single node
-	def nodeThread(node_id, cmd):
-		if(cmd=="join"):
-			join(node_id)
-
-	def find_successor(req_node):
+	def find_successor(self, req_node):
+		print "yay"
 		pass
 		#
-	def find_predecessor(node_id):
+	def find_predecessor(self, node_id):
 		pass
 
 	#this function removes node with the id "node_id" from the system
-	def remove_node(node_id):
+	def remove_node(self, node_id):
 		pass
 		#somehow remove the node
 	#this function finds key_id
-	def find_key(node_id, key_id):
+	def find_key(self, node_id, key_id):
 		pass
 		#somehow find the key
 	
